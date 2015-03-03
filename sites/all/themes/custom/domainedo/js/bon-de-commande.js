3ebd52122a6f29b6b1a28909a8537c452cb91a19
yvandouenel <yvan@coopernet.fr>
2015-03-03 19:23:23 +0100

    bon-de-commande.js added.

---
+++ sites/all/themes/custom/domainedo/js/bon-de-commande.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//alert("hello world !");
+jQuery(function($) {
+  var total = 0;
+  $('<h2 id="h2-total" style="float:left">Total : <span id="total">0€</span></h2>').insertBefore('#edit-submit');//Insére le noeud #h2 avant le noeud # detail
+  $( ".form-radio" ).change(function() {
+    $( "input:checked" ).each(function( index ) {
+      var id = $(this).attr('id');
+      var res = (id.match(/euro[^\.]/g));
+
+      if(res){
+        res = res.toString();
+        res = res.substring(4);
+        total = ($( this ).val()*res)+"€";
+        $("#total").text(total);
+        
+      }
+    }); //end each
+  });
+  
+  
+});
+
+
