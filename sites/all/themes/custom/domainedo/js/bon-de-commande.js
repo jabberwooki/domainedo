29da34728c62322409c00977defb44f5378ddd61
yvandouenel <yvan@coopernet.fr>
2015-03-10 19:04:59 +0100

    bon-de-commande.js modified.

--- sites/all/themes/custom/domainedo/js/bon-de-commande.js
+++ sites/all/themes/custom/domainedo/js/bon-de-commande.js
@@ -5,16 +5,17 @@
  */
 //alert("hello world !");
 jQuery(function($) {
+  console.log("hello");
   var total = 0;
   $('<h2 id="h2-total">Total : <span id="total">0€</span></h2>').insertBefore('#edit-previous');//Insére le noeud #h2 avant le noeud # detail
   $( ".form-radio" ).change(function() {
     $( "input:checked" ).each(function( index ) {
       var id = $(this).attr('id');
-      var res = (id.match(/euro[^\.]/g));
-
+      var res = (id.match(/eu-[^\.]/g));
+      console.log(res);
       if(res){
         res = res.toString();
-        res = res.substring(4);
+        res = res.substring(3);
         total = ($( this ).val()*res)+"€";
         $("#total").text(total);
         $("#edit-submitted-total").val(total);
