2d561f7a2812b0705c5d71c6dfde2032be48bc18
Yvan Douenel <y.douenel@coopernet.fr>
2015-12-15 14:13:36 +0100

    open-close.js up.

--- sites/all/themes/custom/domainedo/js/open-close.js
+++ sites/all/themes/custom/domainedo/js/open-close.js
@@ -19,7 +19,7 @@ jQuery(function($) {
     array_read_more[cpt] = {};
 
     // lien pour fermer les paragraphes
-    array_read_more[cpt].link_close = $( '<a id="link-close-txt-' + cpt + '" href="." class="link-close-txt">Cacher le texte</a>' );
+    array_read_more[cpt].link_close = $( '<a id="link-close-txt-' + cpt + '" href="#link-open-txt-' + cpt + '" class="link-close-txt">Cacher le texte</a>' );
 
     // lien pour ouvrir les paragraphes
     array_read_more[cpt].link_open = $( '<a id="link-open-txt-' + cpt + '" class="read-more" href=".">Lire la suite</a>' );
@@ -31,10 +31,11 @@ jQuery(function($) {
     array_read_more[cpt].wrapper = array_read_more[cpt].p_with_link_read_more.parent();
 
     // récupération du texte avant et après le lien lire la suite ainsi que de l'intitulé du lien
-    array_read_more[cpt].text_before_link = array_read_more[cpt].p_with_link_read_more.html().replace(/<a [^>]*class="read-more".*>(.*)<\/a>(.*)/g, "");
+    array_read_more[cpt].text_before_link = array_read_more[cpt].p_with_link_read_more.html().replace(/<a [^>]*class="read-more".*>(.*)<\/a>(.*)/gi, "");
     array_read_more[cpt].label_link = RegExp.$1;
     array_read_more[cpt].text_after_link = $('<span class="span-after" id="span-after' +cpt+ '">' + RegExp.$2 + '</span>');
     array_read_more[cpt].text_before_link = $('<span class="span-before" id="span-before' +cpt+ '">' + array_read_more[cpt].text_before_link + '</span>');
+
 
     // remplacement du label de base du lien par celui choisi par le rédacteur
     array_read_more[cpt].link_open.text(array_read_more[cpt].label_link);
@@ -73,7 +74,7 @@ jQuery(function($) {
         array_read_more[compteur].text_after_link.slideUp(800);
         array_read_more[compteur].link_open.slideDown(800);
         array_read_more[compteur].next_paragraphes.slideUp(800);
-        return false;
+        /*return false;*/
       });
     })(cpt);
 
