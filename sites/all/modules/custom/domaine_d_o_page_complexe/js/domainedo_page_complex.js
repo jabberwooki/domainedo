07896c8f5b54bff6de73fbc9ed1f2015f039dea3
yvandouenel <y.douenel@coopernet.fr>
2017-01-15 19:52:11 +0100

    domainedo_page_complex.js up.

--- sites/all/modules/custom/domaine_d_o_page_complexe/js/domainedo_page_complex.js
+++ sites/all/modules/custom/domaine_d_o_page_complexe/js/domainedo_page_complex.js
@@ -1,17 +1,20 @@
 jQuery(function($){
     // ne s'exécute pas en mode édition
+    var context;
     if(!$('body.page-node-edit').length) {
-        $('.field-name-field-paragraphe-complexe').each(function (index) {
+        $('.field-name-field-paragraphe-complexe .field-item').each(function (index) {
             if ($('.field-name-field-summary-page-complexe', $(this)).length
                 && $('.field-name-field-texte-page-complexe', $(this)).length) {
+                context = $(this);
                 $('.field-name-field-texte-page-complexe', $(this)).hide(0);
-
-                var context = $(this);
                 $('.field-name-field-summary-page-complexe', $(this)).addClass("close-paragraph");
-                $('.field-name-field-summary-page-complexe', $(this)).click(function(event) {
-                    $('.field-name-field-texte-page-complexe', $(context)).toggle(0);
-                    $('.field-name-field-summary-page-complexe', $(context)).toggleClass("open-paragraph").toggleClass("close-paragraph");
-                });
+                (function(c) {
+                    $('.field-name-field-summary-page-complexe', $(context)).click(function(event) {
+                        $('.field-name-field-texte-page-complexe', $(c)).toggle(0);
+                        $('.field-name-field-summary-page-complexe', $(c)).toggleClass("open-paragraph").toggleClass("close-paragraph");
+                    });
+                })(context);
+
             }
         })
     }
