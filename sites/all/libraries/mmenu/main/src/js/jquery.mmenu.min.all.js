4683f45803f1607609a76b72d1bff0c04b6851ca
Yvan Douenel <y.douenel@coopernet.fr>
2018-07-26 12:46:58 +0200

    librarie mmenu added.

--- sites/all/libraries/mmenu/main/src/js/jquery.mmenu.min.all.js
+++ sites/all/libraries/mmenu/main/src/js/jquery.mmenu.min.all.js
@@ -1,8 +1,3 @@
-(function(){
-  var width_ddonew =  document.getElementById('body-ddonew').clientWidth;
-  if(width_ddonew < 768){
-
-
 /*
  * jQuery mmenu v4.7.5
  * @requires jQuery 1.7.0 or later
@@ -86,5 +81,3 @@
  * Copyright (c) Fred Heusschen
  */
 !function(e){var t="mmenu",s="toggles";e[t].addons[s]={_init:function(t){var a=this;this.opts[s],this.conf[s],this.__refactorClass(e("input",t),this.conf.classNames[s].toggle,"toggle"),this.__refactorClass(e("input",t),this.conf.classNames[s].check,"check"),e("input."+c.toggle+", input."+c.check,t).each(function(){var t=e(this),s=t.closest("li"),l=t.hasClass(c.toggle)?"toggle":"check",n=t.attr("id")||a.__getUniqueId();s.children('label[for="'+n+'"]').length||(t.attr("id",n),s.prepend(t),e('<label for="'+n+'" class="'+c[l]+'"></label>').insertBefore(s.children("a, span").last()))})},_setup:function(){},_add:function(){c=e[t]._c,a=e[t]._d,l=e[t]._e,c.add("toggle check"),n=e[t].glbl}},e[t].defaults[s]={},e[t].configuration.classNames[s]={toggle:"Toggle",check:"Check"};var c,a,l,n}(jQuery);
-  }
-  })();
