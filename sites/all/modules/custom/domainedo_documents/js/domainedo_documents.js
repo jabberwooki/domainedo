e44fee5f76de7e2474ab6f0783715d166035b661
Yvan Douenel <y.douenel@coopernet.fr>
2018-08-28 12:07:26 +0200

    domainedo_documents.js up to delete matchHeight call.

--- sites/all/modules/custom/domainedo_documents/js/domainedo_documents.js
+++ sites/all/modules/custom/domainedo_documents/js/domainedo_documents.js
@@ -3,7 +3,7 @@
   Drupal.behaviors.needsFull = {
     attach: function (context, settings) {
       console.log("Docs techniques");
-      $('.node-type-tech-document .field-name-field-references-techniques > .field-items > .field-item').matchHeight();
+      //$('.node-type-tech-document .field-name-field-references-techniques > .field-items > .field-item').matchHeight();
     }
   };
 }(jQuery))
