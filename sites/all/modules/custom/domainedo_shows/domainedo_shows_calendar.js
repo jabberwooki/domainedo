249f10f636cf645e4690e755aea67225d8b95c57
Yvan Douenel <y.douenel@coopernet.fr>
2018-11-20 15:18:58 +0100

    Less and js up to manage calendar display.

--- sites/all/modules/custom/domainedo_shows/domainedo_shows_calendar.js
+++ sites/all/modules/custom/domainedo_shows/domainedo_shows_calendar.js
@@ -1,24 +1,58 @@
 jQuery(function($) {
   var container_points;
+  var container_shows;
   var nb_points;
   var width_container_points;
+  var right_date;
   $(".single-day div.inner").each(function(index) {
     nb_points = $(".view-item", $(this)).length;
+
     if(nb_points) {
       // création du container des points avec centrage dans la case
       width_container_points = (nb_points > 6) ? 6 : nb_points
       container_points = $("<div></div>",{
         "class": "group-show-points-domainedo group-size-"+ width_container_points,
-        "id": "group-show-points-domainedo-" + index,
-      }).appendTo($(this));
+        "id": "group-show-domainedo-" + index,
+      }).appendTo($(this)).click(function() {
+        // Cache tous les groupes de spectacles de droite
+        $(".group-show-domainedo").hide(0);
+        // affiche le groupe concerné
+        $("."+$(this).attr('id')).show();
+      });
 
-      // création des points et placement dans le container
+      // création d'un groupe avec les spectacles
+      container_shows = $("<div></div>",{
+        "class": "group-show-domainedo group-show-domainedo-" + index,
+      });
+
+      // Parcours des spectacles
       $(".view-item", $(this)).each(function(num) {
+        // récupérer la date sur le premier spectacle
+        if(num == 0) {
+          right_date = $(".date-full-domainedo",$(this)).text();
+          right_date = $("<div></div>",{
+            "class": "right-date",
+            "text": right_date
+          }).appendTo(container_shows);
+        }
+
+        // création des points et placement dans le container de points
         $("<div></div>",{
           "class": "item-show-point-domainedo",
         }).appendTo(container_points);
-        $(this).appendTo("#calendar-detail-domainedo");
+
+        // Placement des spectacles dans le container de spectacles et déplacement de ce dernier
+        $(this).appendTo(container_shows);
       });
+
+      // Positionnement du container de spectacles dans la colonne de droite
+      container_shows.addClass("group-show-domainedo-" + index).appendTo("#calendar-detail-domainedo");
+
+      // Cache tous les groupes de spectacles de droite
+      $(".group-show-domainedo").hide(0);
+
+      // Affiche le premier groupe de spectacles de droite
+      $(".group-show-domainedo:first").show(0);
 
     }
   });
