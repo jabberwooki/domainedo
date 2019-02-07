d989753594c85e046c9077a57e1cdb7d140b7eb5
Yvan Douënel <y.douenel@coopernet.fr>
2019-02-07 12:30:21 +0100

    domainedo_shows.js up to add social buttons.

--- sites/all/modules/custom/domainedo_shows/domainedo_shows.js
+++ sites/all/modules/custom/domainedo_shows/domainedo_shows.js
@@ -12,4 +12,36 @@ jQuery(function($) {
     if($(".field-name-field-calendar-archive").length){
         if ($(".field-name-field-calendar-archive a").attr('href') == '/calendrier') $(".field-name-field-calendar-archive a").hide();
     }
+    // facebook
+    console.log("facebook");
+    const pathname = window.location.pathname;
+    const facebook_button = 
+    $('<div class="fb-like" data-href="https://www.domainedo.fr' +
+    pathname +
+    '" data-width="40" data-layout="button" data-action="recommend" data-size="large" data-show-faces="false" data-share="true"></div>');
+    facebook_button.insertAfter(".field-name-field-by");
+
+   
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v3.2&appId=1815308221819289&autoLogAppEvents=1';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+
+    //Twitter
+    const twitter_url = 'https://twitter.com/intent/tweet?url=https%3A%2F%2Fdomainedo.fr'+
+    pathname +
+    '&text=Je+vous+suggère&hashtags=spectacle,Montpellier';
+    
+    const twitter_button = $('<a></a>',{
+        text: "lien",
+        class: "icon-twitter",
+        href: twitter_url
+    }).insertAfter(facebook_button);  
+
+    // SMS
+    const sms_link = $('<a href="sms:?&body=Je suggère ' + 'https://www.domainedo.fr' + pathname + '">SMS</a>');
+    sms_link.insertAfter(twitter_button);  
 });
