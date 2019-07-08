58774429e4ed14605daaae075bb130dedb179e22
Christophe Espiau <christophe.espiau@gmail.com>
2019-07-08 18:00:11 +0200

    domainedo_webpush up: Testing iOS Platform added.

--- sites/all/themes/custom/ddobs/less/template.php
+++ sites/all/themes/custom/ddobs/less/template.php
@@ -4,6 +4,9 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+use phpbrowscap\Browscap;
+
 function ddobs_menu_tree($variables) {
   $menu_type = str_replace('menu_tree__menu_', '', $variables['theme_hook_original']);
   return '<ul class="menu ' . str_replace(array('_', ' '), '-', strtolower($menu_type)) . '-menu">' . $variables['tree'] . '</ul>';
@@ -20,4 +23,15 @@ function ddobs_preprocess_page(&$variables, $hook) {
   if(drupal_is_front_page()) {
     drupal_add_js(drupal_get_path('theme', 'ddobs') . '/js/festivals_front.js');
   }
+
+  $cache_path = drupal_get_path('module', 'domainedo_webpush') . '/cache/';
+  $bc = new Browscap($cache_path);
+  $bc->remoteIniUrl = 'http://browscap.org/stream?q=Lite_PHP_BrowsCapINI';
+  $current_browser = $bc->getBrowser();
+
+  $webpush_compatibility = TRUE;
+  if ($current_browser->Platform == 'iOS') {
+    $webpush_compatibility = FALSE;
+  }
+  $variables['webpush_compatibility'] = $webpush_compatibility;
 }
